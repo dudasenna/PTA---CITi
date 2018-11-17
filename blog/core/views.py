9b293eb617720fde7e8c0f463c4c8dbe4e60ab8d
dudasenna <mesos@cin.ufpe.br>
2018-11-17 17:03:23 -0200

    meu primeiro commit

---
+++ blog/core/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from core.models import Post
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from templated_email import send_templated_mail
+# Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all().order_by('-created_date')
+        return context
+
+
+def ContactView(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        send_templated_mail(
+            template_name = 'email', #nome do arquivo email.email
+            from_email = 'email',
+            recipient_list = ['mesos@cin.ufpe.br'], #email que vai receber as mensagens, por exemplo, a EJ
+            context={
+                'nome': name,
+                'email': email,
+                'mensagem': message,
+            }
+        )
+
+        return HttpResponseRedirect(reverse_lazy('home'))
+
+    return(render(request, 'contact.html'))
+    
